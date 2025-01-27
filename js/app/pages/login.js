8486b9c2e996b3eddbaca4efe3ed8ca542a691be
maxgleba <166616971+maxgleba@users.noreply.github.com>
2025-01-27 17:13:33 +0200

    Add files via upload

---
+++ js/app/pages/login.js
@@ -0,0 +1,23 @@
+export const login = {
+    data: function() {
+        return {
+            img: 1,
+            hs: 0,
+            parent: ''
+        };
+    },
+    mounted: function() {
+        this.img = this.randomIntFromInterval(1, 7);
+        this.parent = this.$parent.$parent;
+    },
+    methods: {
+        randomIntFromInterval(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+    },
+    template: `
+        <div class="flex">
+            gdfgdfgdfgdf
+        </div>
+    `
+};
