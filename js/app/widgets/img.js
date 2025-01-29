53f1fdea7cb070f5b7d6457e02c02fbf06ea19ef
maxgleba <166616971+maxgleba@users.noreply.github.com>
2025-01-29 19:52:10 +0200

    Update img.js

--- js/app/widgets/img.js
+++ js/app/widgets/img.js
@@ -1,13 +1,22 @@
 export const img = {
     data() {
         return {
-            src: "default.jpg"
+            src: ""
         };
     },
     methods: {
-        changeImage(newSrc) {
-            this.src = newSrc;
+        changeImage() {
+            const images = [
+                "https://source.unsplash.com/random/800x600",
+                "https://picsum.photos/800/600",
+                "https://placekitten.com/800/600",
+                "https://placebear.com/800/600"
+            ];
+            this.src = images[Math.floor(Math.random() * images.length)];
             console.log("Image changed to:", this.src);
         }
+    },
+    mounted() {
+        this.changeImage(); // Меняем картинку при загрузке компонента
     }
 };
